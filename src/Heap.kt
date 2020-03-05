beb970e6c1eb6a1878c0a9781f234b6b405f78b7
JonasGroenbek <jonasgroenbek@gmail.com>
2020-03-05 20:42:18 +0100

    cleanup

--- src/Heap.kt
+++ src/Heap.kt
@@ -10,7 +10,8 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
     }
 
     /**
-     * performing a heapsort, returning an array
+     * performing a heapsort
+     * @return a full Array<T> without null elements
      */
     fun sort() : Array<T> {
         for(partition in size - 1 downTo 0) {
@@ -23,17 +24,10 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
         return sortedHeap as Array<T>
     }
 
-    //only for testing
-    private fun print() {
-        if(size == 0) return
-        for(pos in 0 until size){
-            println(heap[pos])
-        }
-    }
-
     /**
+     * NOTE - could have replaced the heapify calls with swim/sink had they not been recursive.
+     * That is probably a better solution since heapify has to be wrapped in a loop.
      * @param pos the position of the root of which subtree to heapify
-     * NOTE - could have replaced the heapify calls with swim/sink if they weren't recursive and made the heapify recursive
      */
     private fun heapify(pos: Int){
         //basecase
@@ -55,21 +49,9 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
          }
     }
 
-    //Magic
-    private fun swap(pos1: Int, pos2: Int) {
-        heap[pos1] = heap[pos2].also { heap[pos2] = heap[pos1] }
-    }
-
     /**
-     * ArrayIndexOutOfBounds probably should not be handled here, but for now it makes sense
-     */
-    private fun lessThan(pos1: Int, pos2: Int) : Boolean {
-        return if (pos2 > size || heap[pos2] == null) false
-               else                                   heap[pos1]?.compareTo(heap[pos2]!!)!! < 0
-    }
-
-    /**
-     * probably can be made cleaner
+     * This is used as a check whether a sink/heapify should be sustained
+     * NOTE - could probably be made a lot cleaner.
      */
     private fun shouldSustain(pos: Int, partition: Int): Boolean {
         if(isLeaf(pos) || size == 0 || leftChild(pos) > partition) return true
@@ -78,40 +60,7 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
     }
 
     /**
-     * Increasing the size of the array if possible
-     */
-    private fun insert(e: T){
-        increaseSize()
-        heap[size] = e
-        swim(size - 1)
-        size++
-    }
-
-    /**
-     * this will leave a dead element in the array, which is what the size partition is used for
-     */
-    fun extract() : T {
-        val e = heap[0]
-        swap(0, --size)
-        heap[size] = null
-        sink(0, size)
-        return e as T
-    }
-
-
-    private fun allocate(): Int = if (size == 0)  1 else size * 2
-
-    /**
-     * increases the amount of elements the array allocates if it is capped
-     */
-    private fun increaseSize(){
-        if(size == heap.size || size == 0){
-            heap = heap.copyOf(allocate())
-        }
-    }
-
-    /**
-     * If the heap is not heapified before swimming, neither will it be after.
+     * Recursively swim an element up the heap to maintain a heapifified state
      */
     private fun swim(pos: Int){
         //basecase
@@ -122,7 +71,7 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
     }
 
     /**
-     * Recursive function that sinks a element down the tree, prioritizing swapping the biggest of children
+     * Recursively sink an element down the tree, prioritizing swapping the larger child
      * @param pos the position of the element to sink
      * @param partition defines the position to scope the sink to. For full sink size - 1
      */
@@ -144,6 +93,50 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
         }
     }
 
+    //Magic
+    private fun swap(pos1: Int, pos2: Int) {
+        heap[pos1] = heap[pos2].also { heap[pos2] = heap[pos1] }
+    }
+
+    /**
+     * NOTE - ArrayIndexOutOfBounds probably should not be handled here, but for now it makes sense
+     * @param pos1 position of element that checks if it is less than the other return true
+     * @param pos2 position of element that checks if it is greater than the other return true
+     */
+    private fun lessThan(pos1: Int, pos2: Int) : Boolean {
+        return if (pos2 > size || heap[pos2] == null) false
+        else                                   heap[pos1]?.compareTo(heap[pos2]!!)!! < 0
+    }
+
+    fun insert(e: T){
+        increaseSize()
+        heap[size] = e
+        swim(size - 1)
+        size++
+    }
+
+    private fun allocate(): Int = if (size == 0)  1 else size * 2
+
+    /**
+     * increases the amount of elements the array allocates if it is currently capped
+     */
+    private fun increaseSize(){
+        if(size == heap.size || size == 0){
+            heap = heap.copyOf(allocate())
+        }
+    }
+
+    /**
+     * Extracting and returning the top element from the heap, setting empty value to null
+     */
+    fun extract() : T {
+        val e = heap[0]
+        swap(0, --size)
+        heap[size] = null
+        sink(0, size)
+        return e as T
+    }
+
     private fun rightChild(pos: Int) : Int{
         return (pos + 1) * 2
     }
@@ -160,20 +153,26 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
         return (size - 1) / 2 < pos
     }
 
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val arr = arrayOf(10,34,23,5,23,4567,34,23423,764)
-            val heap = Heap(arr, 6)
-            
-            heap.insert(278)
-            heap.extract()
-            heap.insert(12)
-
-            val sorted : Array<Int> = heap.sort()
-
-            for(el in 0 until sorted.size){
-                println(sorted[el])
-            }
+    //only for testing
+    private fun print() {
+        if(size == 0) return
+        for(pos in 0 until size){
+            println(heap[pos])
         }
+    }
+}
+
+fun main() {
+    val arr = arrayOf(10,34,23,5,23,4567,34,23423,764)
+    val heap = Heap(arr, 6)
+
+    heap.insert(278)
+    heap.extract()
+    heap.insert(12)
+
+    val sorted : Array<Int> = heap.sort()
+
+    for(el in 0 until sorted.size){
+        println(sorted[el])
     }
 }
