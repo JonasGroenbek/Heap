9d0d094d95609394db49084158abe90794556bd0
JonasGroenbek <jonasgroenbek@gmail.com>
2020-03-05 19:26:05 +0100

    sort

--- src/Heap.kt
+++ src/Heap.kt
@@ -1,34 +1,34 @@
-class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
+class Heap <T : Comparable<T>>(private var data: Array<T>, private var size: Int) {
 
     //had a lot of problems when using T?
-    private var heap = Array<Any?>(size){ data[it] }
-
+    private var heap : Array<T?>
     /**
      * heapifies all subtrees, excluding leafs
      */
     init {
         if (size > data.size) error("Initial array is too small for initial size.")
+        this.heap = data.sliceArray(0..size) as Array<T?>
+        this.size = size
         for(pos in ((size - 1)/ 2) downTo 0) heapify(pos)
+        print()
     }
 
-    @Suppress("UNCHECKED_CAST")
-    private operator fun get(index: Int) = heap[index] as T
-
-    fun sort(partition: Int) : Array<T> {
-        swap(0, partition)
-        sortElement(0, size)
-        val sortedHeap = heap.filterNotNull() as Array<T>
+    fun sort() : Array<T> {
+        for(partition in size - 1 downTo 0) {
+            swap(0, partition)
+            sink(0, partition -1)
+        }                            
+        val sortedHeap = heap.sliceArray(0..size)
         heapify(0)
-        return sortedHeap
-    }
-
-    private fun sortElement(pos: Int, partition: Int){
-
+        print()
+        //return sortedHeap as Array<T>
+        return sortedHeap as Array<T>
     }
 
     //only for testing
     private fun print() {
-        for(pos in 0..size){
+        if(size == 0) return
+        for(pos in 0 until size){
             println(heap[pos])
         }
     }
@@ -40,11 +40,15 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
      */
     private fun heapify(pos: Int){
         //basecase
-        if(isHeap(pos)) return
+        if(isHeap(pos, size - 1)) return
 
         val lc = leftChild(pos)
         val rc = rightChild(pos)
 
+        if(rc > size - 1){
+            swap(lc, pos)
+            return
+        }
          if(lessThan(lc, rc)){
              swap(rc, pos)
              heapify(rc)
@@ -63,17 +67,16 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
      * ArrayIndexOutOfBounds probably should not be handled here, but for now it makes sense
      */
     fun lessThan(pos1: Int, pos2: Int) : Boolean {
-        return if (pos2 > size || get(pos2) == null) false
-               else                         get(pos1).compareTo(get(pos2)) < 0
+        return if (pos2 > size || heap[pos2] == null) false
+               else                         heap[pos1]?.compareTo(heap[pos2]!!)!! < 0
     }
 
     /**
      * probably can be made cleaner
      */
-    private fun isHeap(pos: Int): Boolean {
-        if(isLeaf(pos) || size == 0) return true;
-        println(pos)
-        return if(rightChild(pos) > size)   (lessThan(leftChild(pos), pos))
+    private fun isHeap(pos: Int, partition: Int): Boolean {
+        if(isLeaf(pos) || size == 0 || leftChild(pos) > partition) return true
+        return if(rightChild(pos) > partition)   (lessThan(leftChild(pos), pos))
                else                         (lessThan(leftChild(pos), pos) && (lessThan(rightChild(pos), pos)))
     }
 
@@ -91,7 +94,7 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
      * this will leave a dead element in the array, which is what the size partition is used for
      */
     private fun extract() : T? {
-        val e = get(0)
+        val e = heap[0]
         swap(0, size -1)
         heap[size - 1] = null
         size--
@@ -100,14 +103,14 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
     }
 
 
-    private fun Array<Any?>.allocate(): Int = if (size == 0)  1 else size * 2
+    private fun Array<T?>.allocate(): Int = if (size == 0)  1 else size * 2
 
     /**
      * increases the amount of elements the array allocates if it is capped
      */
     private fun increaseSize(){
         if(size == heap.size || size == 0){
-            var enlargedHeap = arrayOfNulls<Any>(heap.allocate())
+            val enlargedHeap = arrayOfNulls<Comparable<*>>(heap.allocate()) as Array<T?>
             System.arraycopy(heap, 0, enlargedHeap, 0, heap.size)
             heap = enlargedHeap
         }
@@ -129,7 +132,7 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
      */
     private fun sink(pos: Int, partition: Int){
         //basecase
-        if(isHeap(pos)) return
+        if(isHeap(pos, partition)) return
         if(rightChild(pos) > partition){
             swap(pos, leftChild(pos))
         } else {
@@ -162,19 +165,19 @@ class Heap <T : Comparable<T>>(data: Array<T>, private var size: Int) {
 
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            var arr = arrayOf("asd","asd","345")
-            var heap = Heap(arr, 0)
-            heap.insert("hey")
-            heap.insert("asd")
-            heap.insert("ass")
-            heap.insert("x")
-            heap.insert("z")
-            heap.insert("a")
-            heap.insert("b")
-            heap.insert("xxx")
-            heap.insert("asdasd")
-            heap.insert("xxxxx")
-            heap.print()
+            var arr = arrayOf(10,34,23,5,23,4567,34,23423,764)
+            var heap = Heap(arr, 6)
+            heap.insert(1)
+            heap.insert(3)
+            heap.insert(4)
+            heap.insert(6)
+            heap.insert(5)
+            heap.insert(3)
+            heap.insert(278)
+            heap.insert(32)
+            heap.insert(43)
+            heap.insert(23)
+            heap.sort()
         }
     }
 }
